cbb047305fd04a0f30ff1a171b034b834b3930d0
eshaan <eshaan2031@icloud.com>
2025-05-23 17:24:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js');
 }
+
+fetch('random-number-generator-testing.json')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById('w').textContent = data["random-test-w"];
+    document.getElementById('d').textContent = data["random-test-d"];
+  })
+  .catch(err => console.error('Failed to fetch JSON:', err));
